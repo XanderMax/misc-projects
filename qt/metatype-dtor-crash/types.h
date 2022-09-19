2909dc3638c597514ee553f693af80eb5bed2054
Dmytro Redko <diredko@gmail.com>
2022-09-19 20:07:47 +0300

    Add copy assignment

--- qt/metatype-dtor-crash/types.h
+++ qt/metatype-dtor-crash/types.h
@@ -6,12 +6,25 @@
 class MyGadget
 {
     Q_GADGET
+    int* p = nullptr;
 public:
-    MyGadget() {std::cout << "() " << (void*) this << std::endl;}
-    ~MyGadget() {
-        std::cout << "~() " << (void*) this << std::endl;
+    MyGadget() {
+        p = new int(1);
+        std::cout << "()this=" << (void*)this << " p=" <<(void*)p << std::endl;
     }
-    //MyGadget& operator=(MyGadget&&) {return *this;}
+    ~MyGadget() {
+        std::cout << "~()this=" << (void*)this << " p=" <<(void*)p << std::endl;
+        *p = 10;
+        delete p;
+    }
+
+    MyGadget& operator=(const MyGadget& o)
+    {
+        *p = *o.p;
+        std::cout << "(=)this=" << (void*)this << " p=" << (void*)p
+                  << " o=" << (void*)&o << " o.p=" << (void*)o.p << std::endl;
+        return *this;
+    }
 };
 
 class MySingleton : public QObject
